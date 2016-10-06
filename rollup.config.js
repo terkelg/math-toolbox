d757e795effc5dc4e1a48190e9dab369e5a84a39
Terkel Gjervig <terkel@terkel.com>
2016-10-06 02:20:23 +0200

    fix: Build script

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import babel from 'rollup-plugin-babel'
 export default {
   entry: 'src/math-toolbox.js',
   format: 'umd',
-  moduleName: 'math-toolbox',
+  moduleName: 'MathToolbox',
   plugins: [ babel() ],
   dest: 'dist/math-toolbox.js'
 }
