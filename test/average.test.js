66d88e09ff3953b41af2185b122d1733f7829846
Terkel Gjervig <terkel@terkel.com>
2016-10-23 14:18:51 -0700

    test: Missing description fixed

--- test/average.test.js
+++ test/average.test.js
@@ -1,7 +1,7 @@
 import { average } from '../src/math-toolbox'
 
 describe('Calculate average', () => {
-  it('', () => {
+  it('Expect the everage of 10, 10, 10, 20, 30 to be 16', () => {
     expect(average(10, 20, 10, 10, 30)).toBe(16)
   })
 })
