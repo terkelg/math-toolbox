b1c7383dfafccb221920c2f8b389a0c5c2fa4349
Terkel Gjervig <terkel@terkel.com>
2016-10-29 17:13:32 -0700

    test: Fixed description

--- test/difference.test.js
+++ test/difference.test.js
@@ -1,7 +1,7 @@
 import { difference } from '../src/math-toolbox'
 
 describe('Calculate absolute difference', () => {
-  it('', () => {
+  it('Expect difference between 10 and 20 t0 be 10', () => {
     expect(difference(10, 20)).toBe(10)
   })
 })
